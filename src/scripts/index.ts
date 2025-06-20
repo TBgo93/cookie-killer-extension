e0e60eb9051dde4ade9e177ac4f7e13d99114258
TBgo93 <tbaigorria93@gmail.com>
2025-06-20 20:37:41 -0300

    refactor: add logic to remove twitchAds

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,115 +1,82 @@
-interface GlobalState {
-  isStyleRemoved: boolean;
-  isCookiesPopupRemoved: boolean;
-  isAdsPopupRemoved: boolean;
-  attemptStyle: number;
-  attemptCookies: number;
-  attemptAds: number;
-}
-
-interface LoggerProps {
-  type: "logger" | "globalState";
-  method?: string;
-  element?: HTMLElement;
-  [key: string]: any;
-}
-
-type Logger = (props: LoggerProps) => void;
-type RemoverFunc = (props: { globalState: GlobalState }) => void;
-
-var globalState: GlobalState = {
-  isStyleRemoved: false,
-  isCookiesPopupRemoved: false,
-  isAdsPopupRemoved: false,
-  attemptStyle: 1,
-  attemptCookies: 1,
-  attemptAds: 1,
-};
-
-function logger({ type, ...props }: LoggerProps) {
-  chrome.runtime.sendMessage({ type, ...props });
-}
-
-function getElementIfContainText(text: string) {
-  // Filtramos los elementos que contienen el texto y son visibles (display !== 'none')
-  const elements = Array.from(document.body.querySelectorAll<HTMLElement>('*')).filter(element => 
-    element?.textContent?.includes(text) &&
-    window.getComputedStyle(element).display !== 'none'
-  );
-
-  // Verificamos cuál es el primero que está dentro del viewport
-  for (const element of elements) {
-    const rect = element.getBoundingClientRect();
-    if (
-      rect.top >= 0 && rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    ) {
-      // Devuelve el primer elemento que está completamente dentro del viewport
-      return element;
+function deleteTwitchAdOverlay() {
+  try {
+    const closeBtn = document.querySelector<HTMLElement>(".player-overlay-background > div > div > button");
+    if (closeBtn) {
+      closeBtn.click();
+      console.log("Elemento cerrado", { closeBtn });
     }
+  } catch (error) {
+    console.error("Error al clickear elemento cerrar overlay:", { error });
   }
-
-  // Si no hay ningún elemento en el viewport, devolvemos undefined
-  return undefined;
 }
 
-function removeStyleBody({ globalState }: { globalState: GlobalState }) {
-  if (!document.body) {
-    globalState.isStyleRemoved = false;
-    return false;
+function deleteTwitchAdsBanner() {
+  try {
+    const elementAd = document.querySelector<HTMLElement>(".stream-display-ad__container_lower-third");
+    const elementVideo = document.querySelector<HTMLElement>("[data-a-target='video-ref']");
+    const elementContainerVideo = document.querySelector<HTMLElement>("[data-a-player-state]");
+
+    if (elementAd && elementVideo && elementContainerVideo) {
+      console.log("Banner eliminado.", { elementAd, elementVideo, elementContainerVideo });
+
+      elementVideo.style.setProperty("width", "100%", "important");
+      elementVideo.style.setProperty("height", "100%", "important");
+
+      elementContainerVideo.style.setProperty("width", "100%", "important");
+      elementContainerVideo.style.setProperty("height", "auto", "important");
+
+      elementAd.style.setProperty("display", "none", "important");
+      elementAd.style.setProperty("height", "auto", "important");
+      elementAd.style.setProperty("width", "auto", "important");
+    }
+  } catch (error) {
+    console.error("Error al eliminar elemento Ad banner:", { error });
   }
-  document.body.style.overflow = "auto !important";
-  globalState.isStyleRemoved = true;
-  return true;
 }
 
-function removeCookiesPopup({ globalState } : { globalState: GlobalState }) {
-  const element = getElementIfContainText(" cookies");
-  logger({ type: "logger", method: "removeCookiesPopup", hasElement: !!element, element });
-
-  if(!element) {
-    globalState.isCookiesPopupRemoved = false;
-    return false;
-  }
-
-  element.remove();
-
-  globalState.isCookiesPopupRemoved = true;
-  return true;
+interface PropsNewMutationObserver {
+  callback: Function;
+  mutationType: MutationRecordType;
+  delay?: number;
 }
 
-function removeAdsPopup({ globalState } : { globalState: GlobalState }) {
-  const element = getElementIfContainText("bloqueador de anuncios");
-  logger({ type: "logger", method: "removeAdsPopup", hasElement: !!element, element });
+function newMutationObserver({
+  callback,
+  mutationType,
+  delay
+}: PropsNewMutationObserver) {
+  console.log("newMutationObserver", { callback, mutationType, delay });
+  return new MutationObserver((mutationsList) => {
+    const hasMutation = mutationsList.some((mutation) => mutation.type === mutationType && (mutation.addedNodes.length > 0));
+    if (!hasMutation) {
+      return;
+    }
 
-  if(!element) {
-    globalState.isAdsPopupRemoved = false;
-    return false;
-  }
-
-  element.remove();
-
-  globalState.isAdsPopupRemoved = true;
-  return true;
+    if (delay) {
+      setTimeout(callback, delay);
+    } else {
+      callback();
+    }
+  });
 }
 
-while(!globalState.isCookiesPopupRemoved && globalState.attemptCookies <= 3) {
-  const isRemoved = removeCookiesPopup({ globalState });
-  if(isRemoved) {
-    const isStyleRemoved = removeStyleBody({ globalState });
-    logger({ type: "logger", method: "while", isStyleRemoved });
-  }
-  globalState.attemptCookies++;
+const twitchAdsObserver = newMutationObserver({
+  callback: deleteTwitchAdOverlay,
+  mutationType: "childList"
+});
+
+const twitchAdsBannerObserver = newMutationObserver({
+  callback: deleteTwitchAdsBanner,
+  mutationType: "childList"
+});
+
+if (!window.location.origin.includes("twitch.tv")) {
+  twitchAdsObserver.disconnect();
+  console.log("Se ha desconectado el observer de twitch ads");
+
+  twitchAdsBannerObserver.disconnect();
+  console.log("Se ha desconectado el observer de twitch ads banner");
 }
 
-while(!globalState.isAdsPopupRemoved && globalState.attemptAds <= 3) {
-  const isRemoved = removeAdsPopup({ globalState });
-  if (isRemoved) {
-    const isStyleRemoved = removeStyleBody({ globalState });
-    logger({ type: "logger", method: "while", isStyleRemoved });
-  }
-  globalState.attemptAds++;
-}
-
-logger({ type: "globalState", globalState });
+twitchAdsObserver.observe(document.body, { childList: true, subtree: true });
+twitchAdsBannerObserver.observe(document.body, { childList: true, subtree: true });
