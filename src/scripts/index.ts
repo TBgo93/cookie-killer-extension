13ad03140cab60b93fd5a85774b1a88f399d3477
TBgo93 <tbaigorria93@gmail.com>
2025-06-23 19:48:24 -0300

    refactor: methods file

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,77 +1,10 @@
-function deleteTwitchAdOverlay() {
-  try {
-    const closeBtn = document.querySelector<HTMLElement>(".player-overlay-background > div > div > button");
-    if (closeBtn) {
-      closeBtn.click();
-      console.log("Elemento cerrado", { closeBtn });
-    }
-  } catch (error) {
-    console.error("Error al clickear elemento cerrar overlay:", { error });
-  }
-}
+import {
+  deleteTwitchAdOverlay,
+  deleteTwitchAdsBanner,
+  newMutationObserver,
+  log
+} from "./methods.ts";
 
-function deleteTwitchAdsBanner() {
-  try {
-    const elementAd = document.querySelector<HTMLElement>(".stream-display-ad__container_lower-third");
-    const elementVideo = document.querySelector<HTMLElement>("[data-a-target='video-ref']");
-    const elementContainerVideo = document.querySelector<HTMLElement>("[data-a-player-state]");
-
-    if (elementAd && elementVideo && elementContainerVideo) {
-      console.log("Banner eliminado.", { elementAd, elementVideo, elementContainerVideo });
-
-      elementVideo.style.setProperty("width", "100%", "important");
-      elementVideo.style.setProperty("height", "100%", "important");
-
-      elementContainerVideo.style.setProperty("width", "100%", "important");
-      elementContainerVideo.style.setProperty("height", "100%", "important");
-
-      elementAd.style.setProperty("display", "none", "important");
-      elementAd.style.setProperty("height", "0", "important");
-      elementAd.style.setProperty("width", "0", "important");
-    }
-  } catch (error) {
-    console.error("Error al eliminar elemento Ad banner:", { error });
-  }
-}
-
-interface PropsNewMutationObserver {
-  callback: Function;
-  mutationType: MutationRecordType;
-  delay?: number;
-  limit?: number;
-}
-
-function newMutationObserver({
-  callback,
-  mutationType,
-  delay,
-  // limit
-}: PropsNewMutationObserver) {
-  console.log("newMutationObserver", { callback, mutationType, delay });
-  // let executionCount = 0;
-
-  return new MutationObserver((mutationsList, obs) => {
-    const hasMutation = mutationsList.some((mutation) => mutation.type === mutationType && (mutation.addedNodes.length > 0));
-    if (!hasMutation) {
-      return;
-    }
-
-    // console.log({ executionCount, limit, callback });
-    // if(limit && executionCount > limit) {
-    //   console.log(`Se ha desconectado el observer luego de superar las ${limit} execuciones.`);
-    //   obs.disconnect();
-    //   return;
-    // }
-
-    // executionCount++;
-
-    if (delay) {
-      setTimeout(callback, delay);
-    } else {
-      callback();
-    }
-  });
-}
 
 const twitchAdsObserver = newMutationObserver({
   callback: deleteTwitchAdOverlay,
@@ -86,11 +19,13 @@ const twitchAdsBannerObserver = newMutationObserver({
 
 if (!window.location.origin.includes("twitch.tv")) {
   twitchAdsObserver.disconnect();
-  console.log("Se ha desconectado el observer de twitch ads");
+  log.debug("Se ha desconectado el observer de twitch ads");
 
   twitchAdsBannerObserver.disconnect();
-  console.log("Se ha desconectado el observer de twitch ads banner");
+  log.debug("Se ha desconectado el observer de twitch ads banner");
 }
 
-twitchAdsObserver.observe(document.body, { childList: true, subtree: true });
-twitchAdsBannerObserver.observe(document.body, { childList: true, subtree: true });
+const [main] = document.getElementsByTagName("main");
+
+twitchAdsObserver.observe(main, { childList: true, subtree: true });
+twitchAdsBannerObserver.observe(main, { childList: true, subtree: true });
